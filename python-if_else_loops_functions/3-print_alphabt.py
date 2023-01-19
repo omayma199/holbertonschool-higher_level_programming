d42ae404239d9bb76815cacc8690c091317f19d1
omayma199 <5752@holbertonstudents.com>
2023-01-19 20:09:52 +0100

    When I was having that alphabet soup, I never thought that it would pay off

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for alpha_letters in range(ord('a'), ord('z')+1):
-    if alpha_letters == ord(e) or alpha_letters == ord(q):
+    if alpha_letters == ord(e) or alpha_letters == ord():
         continue
     print("{:c}".format(alpha_letters), end="")
