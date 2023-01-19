89145c401d59b934042fc1ecac4ddb62f2e71467
omayma199 <5752@holbertonstudents.com>
2023-01-19 20:12:48 +0100

    When I was having that alphabet soup, I never thought that it would pay off

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for alpha_letters in range(ord('a'), ord('z')+1):
+for alpha_letters in range(97, 123):
     if alpha_letters == 113 or alpha_letters 101:
         continue
     print("{:c}".format(alpha_letters), end="")
