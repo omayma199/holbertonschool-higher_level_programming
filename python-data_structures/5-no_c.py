b6943b320b4ad4679cb6fbe60898442f688f2581
omayma199 <5752@holbertonstudents.com>
2023-01-24 14:59:00 +0100

    replace

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -5,5 +5,5 @@ def no_c(my_string):
         for i in new_string:
             if i in "cC":
                 new_string.remove(i)
-        my_string = "".join(new_string)
+        my_string = " ".join(new_string)
     return my_string
