fc3481c09a6064b3eb25bd42d78cd18e02d7c29f
omayma199 <5752@holbertonstudents.com>
2023-01-24 14:57:52 +0100

    replace

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-def no_c(my_string)
-    my_string = ''.join(char for char in my_string if char not in 'Cc')
+def no_c(my_string):
+    if my_string:
+        new_string = list(my_string)
+        for i in new_string:
+            if i in "cC":
+                new_string.remove(i)
+        my_string = "".join(new_string)
     return my_string
