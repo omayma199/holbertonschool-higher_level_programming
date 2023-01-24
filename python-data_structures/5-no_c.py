45e4d5ec0ac73d9f24e287b0ce35b1200501db0e
omayma199 <5752@holbertonstudents.com>
2023-01-24 14:52:38 +0100

    replace

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string)
-my_new_string = my_string.translate( { ord("cC"): None } )
-print(my_new_string)
+    return ("".join(c for c in my_string if c not in "Cc"))
