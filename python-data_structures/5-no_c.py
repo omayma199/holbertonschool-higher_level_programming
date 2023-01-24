ec51ee9a0f2c986fd71cc7f283324d82fcf3eb08
omayma199 <5752@holbertonstudents.com>
2023-01-24 14:54:56 +0100

    replace

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string)
-    return ("".join(c for c in my_string if c not in "Cc"))
+    my_string = ''.join(char for char in my_string if char not in 'Cc')
+    return my_string
