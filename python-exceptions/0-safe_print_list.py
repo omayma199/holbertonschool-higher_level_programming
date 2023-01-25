b985bc0cf3cbfca879fdf12865fa28f02922b8a6
omayma199 <5752@holbertonstudents.com>
2023-01-25 21:03:58 +0100

    firstcommit

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    ctr = 0
-    for i in range(0, x):
+    counter = 0
+    for a in range(0, x):
         try:
             print(my_list[a], end="")
-            ctr = ctr + 1
+            counter = counter + 1
         except IndexError:
             break
     print()
-    return ctr
+    return counter
