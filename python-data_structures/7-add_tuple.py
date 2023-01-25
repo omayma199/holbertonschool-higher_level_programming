b75eb55a1c7c8962372d09c7ebeed8d4021637f0
omayma199 <5752@holbertonstudents.com>
2023-01-25 14:49:25 +0100

    add tuple

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    i = len(tuple_a)
+    j = len(tuple_b)
+
+    sum = ((tuple_a[0] if i > 0 else 0) + (tuple_b[0] if j > 0 else 0),
+            (tuple_a[1] if i > 1 else 0) + (tuple_b[1] if j > 1 else 0))
+
+    return sum
