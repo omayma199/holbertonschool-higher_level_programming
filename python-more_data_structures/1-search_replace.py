6179e0b9063a8982b981546722b787f1f5b07f35
omayma199 <5752@holbertonstudents.com>
2023-01-27 19:09:44 +0100

    search replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for idx, item in enumerate(my_list):
-        if item == 4:
-            my_list[idx] = 44
-    print my_list
+    return [replace if i == search else i for i in my_list]
