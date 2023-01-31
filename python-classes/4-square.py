a769ecb053761b21460c339fbfb982cd837989b7
omayma199 <5752@holbertonstudents.com>
2023-01-31 13:43:12 +0100

    squarre

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -10,9 +10,11 @@ class Square:
     def __init__(self, size=0):
         """Initializes the data."""
         self.size = size
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -24,4 +26,3 @@ class Square:
 
     def area(self):
         return (self.__size)**2
-    
