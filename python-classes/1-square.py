4ed86350d96528d3e90120dda345acb5099eb35e
omayma199 <5752@holbertonstudents.com>
2023-01-31 12:53:28 +0100

    square

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 0-square
+Defines class Square
+"""
+
+
+class Square:
+    """Empty class"""
+    def __init__(self, size):
+        """Initializes the data."""
+        self.size = size
