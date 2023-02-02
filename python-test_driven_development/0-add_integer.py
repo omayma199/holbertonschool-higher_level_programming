2c02f10ea8ae959a0d0c3f8058dcbbeb1c3432cf
omayma199 <5752@holbertonstudents.com>
2023-02-02 20:34:11 +0100

    Integers addition

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """Integers addition"""
+    if not (isinstance(a, int) or isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    elif not (isinstance(b, int) or isinstance(b, float)):
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
