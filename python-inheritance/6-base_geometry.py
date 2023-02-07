390df16d11b91bdf818e28dee99a913395377e01
omayma199 <5752@holbertonstudents.com>
2023-02-07 21:23:19 +0100

    base geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""initializate module"""
+
+
+class BaseGeometry:
+    """create class"""
+    @property
+    def area(self):
+    """public nstance method"""
+        raise Exception("area() is not implemented")
