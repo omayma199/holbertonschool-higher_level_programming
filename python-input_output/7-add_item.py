b0a90fc7b1c9bfef05e21bfcae55f8b652ea3a27
omayma199 <5752@holbertonstudents.com>
2023-02-09 23:10:02 +0100

    add item

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -12,7 +12,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 args = sys.argv
 filename = "add_item.json"
 with open(filename, 'a+', encoding="utf-8") as f:
-    my_list = []
+    my_list = args[1:]
     my_list.extend(args[1:])
     save_to_json_file(my_list, filename)
     load_from_json_file(filename)
