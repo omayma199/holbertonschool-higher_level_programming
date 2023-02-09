69d17b99a1db9bc187988b62c1026712d0a17bcc
omayma199 <5752@holbertonstudents.com>
2023-02-09 22:58:40 +0100

    save json file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """
-function that writes Python obj to file using JSON represenation
+adds all arguments to a Python list, and then save them to a file
 """
+import sys
 
 
-def save_to_json_file(my_obj, filename):
-    """
-    Writes Python obj to file using JSON represenation
-    """
-    import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-    with open(filename, mode="w", encoding="utf-8") as f:
-        json.dump(my_obj, f)
+open("add_item.json", "a")
+try:
+    l = load_from_json_file("add_item.json")
+except ValueError:
+    l = []
+save_to_json_file(l + sys.argv[1:], "add_item.json")
