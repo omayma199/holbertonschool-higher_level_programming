df387ee1a0ed77752ea09cc9829ac4fa064d32f4
omayma199 <5752@holbertonstudents.com>
2023-02-14 14:38:47 +0100

    base

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""base model class."""
+
+class Base:
+    """
+    The Base Class
+    Attributes:
+        __nb_object : private class atribute
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
