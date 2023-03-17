2ab8a5d3e8a8c5232f2858bfe8fc5335282ce3be
oumayma <omayma199@github.com>
2023-03-17 23:27:41 +0100

    map

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
 """
-script that lists all State objects
-from the database hbtn_0e_6_usa
+Created on Sat Aug  8 09:05:11 2020
+@author: Robinson Montes
 """
-
-
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
 import sys
 
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from model_state import Base, State
-
-
-if __name__ == "__main__":
-    engine = create_engine(
-                            'mysql+mysqldb://{}:{}@localhost/{}'
-                            .format(
-                                        sys.argv[1],
-                                        sys.argv[2],
-                                        sys.argv[3]
-                                            ),
-                            pool_pre_ping=True
-                                )
-    session = Session(engine)
-    for state in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
-    session.close()
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) != 4:
+        print("Usage: {} username password database_name".format(args[0]))
+        exit(1)
+    username = args[1]
+    password = args[2]
+    data = args[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, data))
+    # create custom session object class from database engine
+    Session = sessionmaker(bind=engine)
+    # create instance of new custom session class
+    session = Session()
+    for state in session.query(State).order_by(State.id):
+        print('{}: {}'.format(state.id, state.name))
--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
 """
-script that lists all State objects
-from the database hbtn_0e_6_usa"""
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+Created on Sat Aug  8 09:05:11 2020
+@author: Robinson Montes
+"""
 from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+import sys
 
 
-if __name__ == "__main__":
-    engine = create_engine(
-                            'mysql+mysqldb://{}:{}@localhost/{}'
-                            .format(
-                                        sys.argv[1],
-                                        sys.argv[2],
-                                        sys.argv[3]
-                                            ),
-                            pool_pre_ping=True
-                                )
-    session = Session(engine)
-    re = session.query(State).order_by(State.id).first()
-    if re is None:
-        print("Nothing")
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) != 4:
+        print("Usage: {} username password database_name".format(args[0]))
+        exit(1)
+    username = args[1]
+    password = args[2]
+    data = args[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, data))
+    # create custom session object class from database engine
+    Session = sessionmaker(bind=engine)
+    # create instance of new custom session class
+    session = Session()
+    state = session.query(State).order_by(State.id).first()
+    if state is not None:
+        print('{}: {}'.format(state.id, state.name))
     else:
-        print("{}: {}".format(re.id, re.name))
-    session.close()
+        print('Nothing')
