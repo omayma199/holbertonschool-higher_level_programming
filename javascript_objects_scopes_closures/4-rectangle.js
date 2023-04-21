6cc3ba9a133a21a1b90f646164b5e7b3828d1e8a
oumayma <omayma199@github.com>
2023-04-21 22:19:26 +0100

    objects

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,37 +1,37 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
-    }
-  
-    print () {
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
-      }
-    }
-  
-    charPrint (c) {
-      if (c === undefined) {
-        this.print();
-      } else {
-        for (let j = 0; j < this.height; j++) {
-          console.log(c.repeat(this.width));
-        }
-      }
-    }
-  
-    rotate () {
-      const temp = this.width;
-      this.width = this.height;
-      this.height = temp;
-    }
-  
-    double () {
-      this.width *= 2;
-      this.height *= 2;
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
-  module.exports = Rectangle;
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let j = 0; j < this.height; j++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
-const SquareA = require('./5-rectangle.js');
-module.exports = class Square extends SquareA {
-  charPrint (c) {
-    if (c === undefined) {
-      return this.print();
-    } else {
-      let A = '';
-      for (let i = 0; i < this.width; i++) {
-        A += c;
-      }
-      for (let i = 0; i < this.height; i++) {
-        console.log(A);
-      }
+const Rectangle = require('./5-square.js');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c = 'X') {
+    let square = '';
+    for (let j = 0; j < this.width; j++) {
+      square = square + c;
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(square);
     }
   }
-};
+}
+module.exports = Square;
