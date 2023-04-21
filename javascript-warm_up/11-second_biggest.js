f1df94b5dbe5ec07c0f2f7b6b27738d45fddc6ec
oumayma <omayma199@github.com>
2023-04-21 22:02:43 +0100

    warm up

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 function nextBiggest (arr) {
   let max = 0; let result = 0;
- 
+
   for (const value of arr) {
     const nr = Number(value);
- 
+
     if (nr > max) {
       [result, max] = [max, nr];
     } else if (nr < max && nr > result) {
