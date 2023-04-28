0f0a8789488236bde261f33e64c78e9c4ca31141
oumayma <omayma199@github.com>
2023-04-28 22:51:13 +0100

    first commit

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,18 +1,23 @@
-
 #!/usr/bin/node
 
 const request = require('request');
 const url = process.argv[2];
-request.get(url, function (err, response, body) {
-  let count = 0;
-  if (err) {
-    console.log(err);
-  }
-  const data = JSON.parse(body);
-  for (let i = 0; data.results[i] !== undefined; i++) {
-    if (data.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
-      count++;
+
+request(url, function (err, res, body) {
+    if (err) {
+        console.log(err);
+    } else if (res.statusCode === 200) {
+        let films = JSON.parse(body).results;
+        let count = 0;
+        for (let i = 0; i < films.length; i++){
+            for (let j = 0; j < films[i].characters.length; j++){
+                if (films[i].characters[j].includes('/18/')) {
+                    count++;
+                }
+            }
+        }
+        console.log(count);
+    } else {
+        console.log('Invalid')
     }
-  }
-  console.log(count);
-});
+})
